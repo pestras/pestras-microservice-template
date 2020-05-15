63c1c6fe610372520c1114c13697575f2698fe7f
pestras <admin@pestras.com>
2020-05-15 11:35:26 +0300

    update docker-compose and package.json scripts

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import { SERVICE, MICRO, ServiceEvents } from '@pestras/microservice';
 import { LOGLEVEL } from '@pestras/microservice/logger';
+import { Payload } from 'ts-nats';
 
 const PROD = process.env.NODE_ENV === 'production';
+const NATS = process.env.NATS;
 
 @SERVICE({
   version: 0,
-  logLevel: PROD ? LOGLEVEL.ERROR : LOGLEVEL.DEBUG
+  logLevel: PROD ? LOGLEVEL.ERROR : LOGLEVEL.DEBUG,
+  nats: NATS ? { url: NATS, payload: Payload.JSON} : null
 })
 export class Service implements ServiceEvents {
 
